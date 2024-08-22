64d1f0ca2cb081f8d261b72291ff9f43c2bbf03e
Sherlock-Holmes <wuruijie1412@gmail.com>
2024-08-22 20:58:43 +0800

    添加星空

---
+++ js/skystar.js
@@ -0,0 +1,121 @@
+// poem
+var words=[
+    '伤心桥下春波绿',
+    '曾是惊鸿照影来',
+    '当年明月在',
+    '曾照彩云归',
+    '归去来兮',
+    '真堪偕隐',
+    '画船听雨眠',
+    '愿为江水',
+    '安乐如意',
+    '长寿无极',
+    '一岁一礼，一寸欢喜',
+    '岁岁春无事',
+    '相逢总玉颜',
+    '朱颜长似，头上花枝，岁岁年年',
+    '遥扣芳辰，生辰吉乐',
+    '柳絮空缱绻',
+    '南风知不知',
+    '庭下芝兰茂，腰间印绶荣',
+    '从今把定春风笑，且作人间长寿仙',
+    '取次花丛懒回顾',
+    '半缘修道半缘君',
+    '三笑徒然当一痴',
+    '人生若只如初见',
+    '且喜且乐',
+    '且以永日',
+    '此恨不关风与月',
+    '年年约，常相见。但无事，身强健',
+    '愿君长似少年时',
+    '初心不忘乐相知',
+    '今夜何夕',
+    '见此良人',
+    '今年见',
+    '夜夜流光相皎洁',
+    '明年见',
+    '春色如人面',
+    '盛旦欣逢，寿杯重举，祝公千岁',
+    '入骨相思知不知',
+    '多情只有春庭月',
+    '尤为离人照落花',
+    '若有知音见采',
+    '不辞唱遍阳春',
+    '休言半纸无多重',
+    '万斛离愁尽耐担',
+    '夜月一帘幽梦',
+    '和光同尘',
+    '杳霭流玉',
+    '月落星沉',
+    '霞姿月韵',
+    '喜上眉梢',
+    '醉后不知天在水',
+    '满船星梦压星河',
+    '落花人独立',
+    '微雨燕双飞',
+    '掬水月在手',
+    '弄花香满衣',
+    '夜深忽梦少年事',
+    '唯梦闲人不梦君',
+    '垆边人似月',
+    '皓腕凝霜雪',
+    '众里嫣然通一顾',
+    '人间颜色如尘土',
+    '若非群玉山头见',
+    '会向瑶台月下逢',
+    '沉鱼落雁鸟惊喧',
+    '羞花闭月花愁颤',
+    '解释春风无限恨',
+    '沉香亭北倚阑干'
+];
+function randomNum(min,max){
+    var num = (Math.random()*(max-min+1)+min).toFixed(2);
+    return num;
+}
+function init(){
+    let container = document.querySelector('.container');
+    let f = document.createDocumentFragment();
+    words.forEach(w=>{
+    let word_box = document.createElement('div');
+    let word = document.createElement('div');
+        word.innerText = w;
+        word.classList.add('word');
+        word.style.color = '#BAABDA';
+        word.style.fontFamily = '楷体';
+        word.style.fontSize = '20px'
+        word_box.classList.add('word-box');
+        word_box.style.setProperty("--margin-top",randomNum(-40,20)+'vh');
+        word_box.style.setProperty("--margin-left",randomNum(6,35)+'vw');
+        word_box.style.setProperty("--animation-duration",randomNum(8,20)+'s');
+        word_box.style.setProperty("--animation-delay",randomNum(-20,0)+'s');
+        
+        word_box.appendChild(word);
+        f.appendChild(word_box);
+
+
+    })
+    container.appendChild(f);
+}
+window.addEventListener('load',init);
+let textone = document.querySelector('.textone').querySelector('h1');
+      let texttwo = document.querySelector('.texttwo').querySelector('h1');
+      let textthree = document.querySelector('.textthree').querySelector('h1');
+
+      setTimeout(function(){
+        textone.innerHTML = '今晚，整片星空将为你一人闪烁';
+          textone.style.color = '#E8F9FD';
+          textone.style.fontFamily = '楷体'
+          texttwo.style.color = '#E8F9FD';
+          texttwo.style.fontFamily = '楷体'
+          textthree.style.color = '#E8F9FD';
+          textthree.style.fontFamily = '楷体'
+          texttwo.innerHTML = '';
+      },28000)
+      setTimeout(function(){
+        textone.innerHTML = '希望你看着广阔的星空';
+        texttwo.innerHTML = '心里有方向';
+        textthree.innerHTML = '眼中不再迷茫';
+      },112500)
+
+
+ 
